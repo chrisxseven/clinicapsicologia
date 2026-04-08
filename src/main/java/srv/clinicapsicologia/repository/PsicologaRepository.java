4bc5f5d5661fb0c3eedf9be4cad4b4c3402155ed
Soareskest <andersoneshirlene2@gmal.com>
2026-04-07 22:27:07 -0300

    CPSI-14 feat: Criar camada de repository para psicologa

---
+++ src/main/java/srv/clinicapsicologia/repository/PsicologaRepository.java
@@ -0,0 +1,11 @@
+package srv.clinicapsicologia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import srv.clinicapsicologia.datasource.model.Psicologa;
+
+import java.util.Optional;
+
+public interface PsicologaRepository
+        extends JpaRepository<Psicologa, Long> {
+    Optional<Psicologa> findById(Long idPsicologa);
+}
