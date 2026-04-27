ce760e24d186a110e891466a33cab8c10606a71c
chrisxseven <christiandvd2005@gmail.com>
2026-04-26 21:46:11 -0300

    CPSI-9 fix: adicionado campo de tipoTerapia no model Paciente

--- src/main/java/srv/clinicapsicologia/datasource/model/Paciente.java
+++ src/main/java/srv/clinicapsicologia/datasource/model/Paciente.java
@@ -39,6 +39,9 @@ public class Paciente implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate dataCadastro;
 
+    @Column(name = "tipo_terapia")
+    private String tipoTerapia;
+
     public Long getId() {
         return id;
     }
@@ -93,6 +96,14 @@ public class Paciente implements Serializable {
 
     public void setDataCadastro(LocalDate dataCadastro) {
         this.dataCadastro = dataCadastro;
+    }
+
+    public String getTipoTerapia() {
+        return tipoTerapia;
+    }
+
+    public void setTipoTerapia(String tipoTerapia) {
+        this.tipoTerapia = tipoTerapia;
     }
 }
 
