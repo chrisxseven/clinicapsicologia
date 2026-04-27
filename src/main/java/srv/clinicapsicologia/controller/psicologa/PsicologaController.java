9570c137f11fc874eef5da22a7bea8f2283f2c06
victor <victorguilhermefernandes01@gmail.com>
2026-04-26 22:34:17 -0300

    CPSI-3 Feat: Criado endpoint de psicologo

--- src/main/java/srv/clinicapsicologia/controller/psicologa/PsicologaController.java
+++ src/main/java/srv/clinicapsicologia/controller/psicologa/PsicologaController.java
@@ -1,4 +1,51 @@
 package srv.clinicapsicologia.controller.psicologa;
 
+import srv.clinicapsicologia.datasource.model.Psicologa;
+import srv.clinicapsicologia.exception.PsicologaNotFoundException;
+import srv.clinicapsicologia.resource.model.PsicologaResource;
+import srv.clinicapsicologia.service.psicologa.BuscarPsicologaPorIdService;
+import srv.clinicapsicologia.service.psicologa.BuscarPsicologaService;
+import srv.clinicapsicologia.service.psicologa.CadastroPsicologa;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/psi")
 public class PsicologaController {
+
+    @Autowired
+    private BuscarPsicologaService serviceBuscar;
+
+    @Autowired
+    private CadastroPsicologa serviceCadastro;
+
+    @Autowired
+    private BuscarPsicologaPorIdService serviceBuscarPorId;
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping(path = "/psicologas")
+    public List<Psicologa> buscarPsicologas() {
+        return serviceBuscar.buscarTodasAsPsicologas();
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping(path = "/psicologa/id/{id}")
+    public Psicologa buscarPsicologaPorId(
+            @PathVariable (name = "id", required = true) Long id) throws PsicologaNotFoundException {
+        return serviceBuscarPorId.buscarPorId(id);
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping(path = "/psicologa/save")
+    public void salvarPsicologa(@RequestBody PsicologaResource psicologa) {
+        serviceCadastro.cadastro(psicologa);
+
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @DeleteMapping(path = "psicologa/delete/{id}")
+    public void deletarPsicologa(@PathVariable (name = "id", required = true) Long id) throws PsicologaNotFoundException {
+        serviceBuscarPorId.deletarPorId(id);
+    }
 }
